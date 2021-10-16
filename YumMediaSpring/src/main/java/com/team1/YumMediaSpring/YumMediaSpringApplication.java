0de24c002a9cbf9fd4333fb8c145ac06241525c7
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-10-16 15:55:58 -0500

    base project spring

---
+++ YumMediaSpring/src/main/java/com/team1/YumMediaSpring/YumMediaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.team1.YumMediaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumMediaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumMediaSpringApplication.class, args);
+	}
+
+}
