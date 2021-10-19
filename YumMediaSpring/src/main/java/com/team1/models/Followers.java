b4be29734d4c70f08525f3b1e4b2fc93125eac7f
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2021-10-19 09:11:01 -0400

    Added Followers model

---
+++ YumMediaSpring/src/main/java/com/team1/models/Followers.java
@@ -0,0 +1,61 @@
+package com.team1.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="followers")
+
+public class Followers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer referenceId;
+
+    @Column(name = "follower")
+    private Integer follower;
+
+    @Column(name = "following")
+    private Integer following;
+
+    public Followers() {
+
+    }
+
+    public Followers(Integer referenceId, Integer follower, Integer following) {
+        this.referenceId = referenceId;
+        this.follower = follower;
+        this.following = following;
+    }
+
+    public Integer getReferenceId() {
+        return referenceId;
+    }
+
+    public void setReferenceId(Integer referenceId) {
+        this.referenceId = referenceId;
+    }
+
+    public Integer getFollower() {
+        return follower;
+    }
+
+    public void setFollower(Integer follower) {
+        this.follower = follower;
+    }
+
+    public Integer getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Integer following) {
+        this.following = following;
+    }
+
+    @Override
+    public String toString() {
+        return "Followers{" +
+                "referenceId=" + referenceId +
+                ", follower=" + follower +
+                ", following=" + following +
+                '}';
+    }
+}
