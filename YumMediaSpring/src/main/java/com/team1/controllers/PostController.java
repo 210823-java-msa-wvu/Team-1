850fd10e3a2ebf4b6d4fb8978234c6748db86343
syonkeu <sylvestre.yonkeu@revature.com>
2021-10-20 10:36:40 -0600

    Created Post conrtoller and PostServices

--- YumMediaSpring/src/main/java/com/team1/controllers/PostController.java
+++ YumMediaSpring/src/main/java/com/team1/controllers/PostController.java
@@ -1,5 +1,55 @@
 package com.team1.controllers;
 
+import com.team1.models.Posts;
+import com.team1.services.PostServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/post")
 public class PostController {
-    //test to add in packages
+
+       private PostServices postServices; // injected
+
+        @Autowired
+        public PostController (PostServices postService){
+            this.postServices = postService;
+        }
+
+
+
+        //Create: Add a new Posts
+        //
+        @PostMapping(path ="/addPost", consumes = "application/json", produces = "application/json")
+        public void addPost(@RequestBody Posts post){
+
+            postServices.addPost(post);
+        }
+
+        //READ - getAllPosts, getPost. find all posts or a specific post
+        @GetMapping(path ="/findposts")
+        public List<Posts> getAllPosts(){
+            return postServices.getAllPosts();
+        }
+
+        @GetMapping(path ="/findposts/{post_id}")
+        public Posts getPost(@PathVariable Integer post_id){
+            return postServices.getPost(post_id);
+        }
+
+        //UPDATE - updateposts. update a post
+        @PutMapping(path ="/update/{user_id}")
+        public void updatePost (@PathVariable Integer post_id){
+            Posts p = postServices.getPost(post_id);
+            postServices.updatePost(p);
+        }
+
+        //DELETE - deletePost. delete a Post.
+        @DeleteMapping(path ="/deletePost/{post_id}")
+        public void deletePost (@PathVariable Integer post_id){
+            postServices.deletePost(post_id);
+        }
 }
--- YumMediaSpring/src/main/java/com/team1/services/PostServices.java
+++ YumMediaSpring/src/main/java/com/team1/services/PostServices.java
@@ -1,6 +1,38 @@
 package com.team1.services;
 
+import com.team1.models.Posts;
+import com.team1.repositories.PostRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class PostServices {
+
+    @Autowired
+    private PostRepo PostRepo;
+
+    //read
+    public List<Posts> getAllPosts(){
+        return PostRepo.findAll();
+    }
+    //byid
+    public Posts getPost(Integer post_id){return PostRepo.findById(post_id).orElse(null);}
+
+    //create
+    public void addPost(Posts addPost){
+        PostRepo.save(addPost);
+    }
+
+    //update
+    public void updatePost(Posts post){
+        PostRepo.save(Posts);
+    }
+
+    //delete
+    public Posts deletePost(Integer post_id){
+        PostRepo.deleteById(post_id);
+        return null;
+    }
 
     //test
 }
