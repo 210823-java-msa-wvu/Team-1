6e64089d713300c0c8f26c4fde6552af8266427d
Andy Xue <xueandy32@gmail.com>
2021-10-23 12:16:36 -0500

    added path : comments to routing module

--- yummedia/src/app/app-routing.module.ts
+++ yummedia/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 const routes: Routes = [
-  {path: 'post/findposts', component:PostsComponent}
+  {path: 'post/findposts', component:PostsComponent},
+  {path: 'comments', component:CommentsComponent}
 
 ];
 
