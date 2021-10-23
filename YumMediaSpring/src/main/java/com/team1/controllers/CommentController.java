2b0ae5b0f952934a0e98192d714e162664753148
Andy Xue <xueandy32@gmail.com>
2021-10-23 12:11:26 -0500

    comment controller works again

--- YumMediaSpring/src/main/java/com/team1/controllers/CommentController.java
+++ YumMediaSpring/src/main/java/com/team1/controllers/CommentController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 public class CommentController {
 //    private CommentsRepo commentRepo;
 
+//    @Autowired
     private CommentServices commentService;
 
     @Autowired
