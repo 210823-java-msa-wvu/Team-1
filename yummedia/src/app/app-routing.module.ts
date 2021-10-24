4959124e90c406daf42d296456a6c6ff7960f514
Jonathan Alvarado <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2021-10-24 16:16:14 -0700

    updating my branch

--- yummedia/src/app/app-routing.module.ts
+++ yummedia/src/app/app-routing.module.ts
@@ -19,8 +19,8 @@ const routes: Routes = [
   {path: 'login', component:LoginComponent},
   {path: 'registration', component:RegistrationComponent},
 
-  // // otherwise, redirect to home
-  // {path: '**', redirectTo: ''}
+  // otherwise, redirect to home
+  {path: '**', redirectTo: ''}
 
 ];
 
