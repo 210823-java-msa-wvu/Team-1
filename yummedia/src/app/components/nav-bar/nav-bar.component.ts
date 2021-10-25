4b1d3540aa1d553e897e8766309b15280505703e
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2021-10-25 11:34:03 -0400

    added modal for editting profile

--- yummedia/src/app/components/nav-bar/nav-bar.component.ts
+++ yummedia/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { userInfo } from 'os';
+import { User } from '../../models/user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  public user: User;
   
-  
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) {
+    this.user = authenticationService.currentUserValue;
+  }
 
   ngOnInit(): void {
   }
@@ -20,6 +25,17 @@ export class NavBarComponent implements OnInit {
 
   public editProfile() {
 
+  }
+
+  public openModal() {
+    const navbar = document.getElementById('navbar');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#editProfileModal');
+    navbar?.appendChild(button);
+    button.click();
   }
 
 }
